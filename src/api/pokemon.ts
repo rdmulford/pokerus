ccfeb7539c91d4fdcc50b9578292a4a6cf2a78ef
Kellen Mulford <40723884+mulfordkl@users.noreply.github.com>
2021-12-22 11:28:38 -0600

    fix: return Pokemon list
    Co-authored-by: Riley Mulford <rdmulfor@mtu.edu>

--- src/api/pokemon.ts
+++ src/api/pokemon.ts
@@ -6,7 +6,7 @@ export const getPokemonList = async () => {
   const api = new PokemonClient({
     cacheOptions: {maxAge: 5000, exclude: {query: false}},
   });
-  await api
+  return await api
     .listPokemons(0, 1200)
     .then(data => {
       return data.results;

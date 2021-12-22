4a4ee22d278765be72ac5b18665931ff7cda6f7a
Riley Mulford <rdmulfor@mtu.edu>
2021-12-22 12:56:07 -0600

    feat: add view manager

--- App.tsx
+++ App.tsx
@@ -1,32 +1,14 @@
-import {Berries} from 'pokenode-ts';
 import React from 'react';
-import {
-  NativeBaseProvider,
-  useColorMode,
-  useColorModeValue,
-  Center,
-  Button,
-} from 'native-base';
-import {getBerry} from './src/api/berry';
+import {NativeBaseProvider} from 'native-base';
 import Footer from './src/components/Footer';
 import GlobalStateProvider from './src/components/Global';
-
-function UseColorMode() {
-  const {toggleColorMode} = useColorMode();
-  return (
-    <Center flex="1" bg={useColorModeValue('warmGray.50', 'coolGray.800')}>
-      <Button onPress={toggleColorMode}>Toggle</Button>
-    </Center>
-  );
-}
+import ViewManager from './src/components/ViewManager';
 
 const App = () => {
-  getBerry(Berries.AGUAV);
-
   return (
     <NativeBaseProvider>
       <GlobalStateProvider>
-        <UseColorMode />
+        <ViewManager />
         <Footer />
       </GlobalStateProvider>
     </NativeBaseProvider>

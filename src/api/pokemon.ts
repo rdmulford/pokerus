b9683138caee593bd8c69ef88a96eefd5544bf7f
Kellen Mulford <40723884+mulfordkl@users.noreply.github.com>
2021-12-22 11:47:40 -0600

    Pokemon List (#5)
    * chore: get Pokemon list
    
    * fix: change function name
    
    * Update src/api/pokemon.ts
    
    Co-authored-by: Riley Mulford <rdmulfor@mtu.edu>
    
    * fix: return Pokemon list
    
    Co-authored-by: Riley Mulford <rdmulfor@mtu.edu>
    
    * fix: add function return type
    
    Co-authored-by: Riley Mulford <rdmulfor@mtu.edu>
    
    Co-authored-by: Riley Mulford <rdmulfor@mtu.edu>

---
+++ src/api/pokemon.ts
@@ -0,0 +1,15 @@
+import {PokemonClient} from 'pokenode-ts';
+
+// getPokemonList returns a list of all pokemon
+// TODO currently just prints stuff to console
+export const getPokemonList = async (): Promise<void | NamedAPIResource[] | APIResource[]> => {
+  const api = new PokemonClient({
+    cacheOptions: {maxAge: 5000, exclude: {query: false}},
+  });
+  return await api
+    .listPokemons(0, 1200)
+    .then(data => {
+      return data.results;
+    })
+    .catch(error => console.error(error));
+};

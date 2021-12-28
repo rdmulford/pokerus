533ae91d8362a3085c3633241a4fa1f14c955f35
Riley Mulford <rdmulfor@mtu.edu>
2021-12-27 19:38:47 -0600

    fix: remove warning ignore

--- App.tsx
+++ App.tsx
@@ -2,11 +2,6 @@ import React from 'react';
 import {NativeBaseProvider} from 'native-base';
 import {NavigationContainer} from '@react-navigation/native';
 import TabNavigator from 'components/TabNavigator';
-import {LogBox} from 'react-native';
-
-LogBox.ignoreLogs([
-  'Non-serializable values were found in the navigation state',
-]);
 
 const App = () => {
   return (
